99d2fbce8d258dfc7bff35599855e3a1f9043f73
quduss <shuaib.qudus@yahoo.com>
2025-02-27 14:51:35 +0100

    Write more tests

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -7,6 +7,17 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        node = LeafNode("p", "Hello, world!", {"class": "red-box", "id": "first"})
+        self.assertEqual(node.to_html(), '<p class="red-box" id="first">Hello, world!</p>')
+        node = LeafNode(None, "empty tag")
+        self.assertEqual(node.to_html(), "empty tag")
+    
+    def test_error(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
 
 
 if __name__ == "__main__":
