2edc052c816a0dcadfbf0bae86789b4db9ef6116
quduss <shuaib.qudus@yahoo.com>
2025-02-27 22:25:02 +0100

    Write more tests

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -12,7 +12,20 @@ class TestParentNode(unittest.TestCase):
             LeafNode(None, "Normal text")
         ])
         self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
-        
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+    
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
 
 
