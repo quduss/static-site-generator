6d935f4bebb257f6d58a73f0629f865992686c92
quduss <shuaib.qudus@yahoo.com>
2025-03-02 17:08:06 +0100

    Write function that takes a block of markdown text as input and returns the BlockType it is

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -15,3 +15,29 @@ def markdown_to_blocks(markdown):
         if block != '':
             new_blocks.append(block)
     return new_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
