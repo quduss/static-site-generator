5543349c7d8504c85d7196e27c42f88d05322477
quduss <shuaib.qudus@yahoo.com>
2025-03-02 17:04:08 +0100

    Create a BlockType enum for different block types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = list(map(lambda block: block.strip(), markdown.split('\n\n')))
     new_blocks = []
