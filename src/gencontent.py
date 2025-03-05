9adf985ef75e613840925a67fa4b830d9d04e20a
quduss <shuaib.qudus@yahoo.com>
2025-03-05 13:01:11 +0100

    Write function that generates all pages from all markdowns in content/

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,6 @@
 import os
 from block_markdown import markdown_to_html_node
+from pathlib import Path
 
 def extract_title(md):
     lines = md.split("\n")
@@ -30,3 +31,13 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
